17672d8f4d365b204f27cfc11ddb6b0247fe6758
bobbyviki <rong.cai@viki.com>
2022-03-08 15:54:47 +0800

    Driver file edit

--- driver/driver.py
+++ driver/driver.py
@@ -19,10 +19,7 @@ class Driver:
     def _initialise(self):
         print("Initiating dirver")
         self.ol = Overlay(BIT_PATH)
-        # self.hls_ip = self.ol.Hls_accel_0
         self.dma = self.ol.axi_dma_0
-
-        # self.hls_ip.write(0x00,0x01)
 
         print("Allocated buffer")
         self.input_buffer = allocate(shape=(240,), dtype=np.float32)
