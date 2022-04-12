e1c1e3fb5c500c01af4cf19d7ea813d0083c4ee9
bobbyviki <rong.cai@viki.com>
2022-04-12 10:55:03 +0800

    Edited new driver

--- driver/driver.py
+++ driver/driver.py
@@ -79,7 +79,7 @@ class Driver:
                 correct += 1
         return correct,total
 
-    def softmax(x):
+    def softmax(self, x):
         """Compute softmax values for each sets of scores in x."""
         e_x = np.exp(x - np.max(x))
         return e_x / e_x.sum()        
